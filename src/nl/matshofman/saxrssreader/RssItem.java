f304989566e85cfbb57c758eecdb83b1bafa64ae
Mats Hofman <matshofman@gmail.com>
2011-12-06 21:18:00 +0100

    Added license information and fixed issue #1

--- src/nl/matshofman/saxrssreader/RssItem.java
+++ src/nl/matshofman/saxrssreader/RssItem.java
@@ -1,8 +1,25 @@
+/*
+ * Copyright (C) 2011 Mats Hofman <http://matshofman.nl/contact/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package nl.matshofman.saxrssreader;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 import android.os.Bundle;
 import android.os.Parcel;
@@ -94,7 +111,7 @@ public class RssItem implements Comparable<RssItem>, Parcelable {
 
 	public void setPubDate(String pubDate) {
 		try {
-			SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z");
+			SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
 			this.pubDate = dateFormat.parse(pubDate);
 		} catch (ParseException e) {
 			e.printStackTrace();
